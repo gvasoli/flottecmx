78cbd8d79b65faf95882aaac4d24c52e4f43d8bd
gvasoli <77854767+gvasoli@users.noreply.github.com>
2021-09-02 18:10:04 -0600

    Update mainSim.js

--- assets/js/mainSim.js
+++ assets/js/mainSim.js
@@ -582,14 +582,14 @@ function efectoDepresores(){
 
 
     efectoZNS04Plata = (-4 ) * (1-Math.exp(-0.03*txtSulfatoZincVR));
-    efectoCianuroPlata = (-10) * (1-Math.exp(-0.06*txtCianuroVR));
+    efectoCianuroPlata = (-10) * (1-Math.exp(-0.01*txtCianuroVR));
     efectoDepresoresPlata = efectoZNS04Plata + efectoCianuroPlata;
-    efectoCianuroOro = (-15) * (1-Math.exp(-0.07*txtCianuroVR));
+    efectoCianuroOro = (-15) * (1-Math.exp(-0.09*txtCianuroVR));
     efectoDepresoresOro = efectoZNS04Oro + efectoCianuroOro;
     efectoZNS04Zinc = (-13 ) * (1-Math.exp(-0.055*txtSulfatoZincVR));
-    efectoCianuroZinc = (-6) * (1-Math.exp(-0.1*txtCianuroVR));
+    efectoCianuroZinc = (-6) * (1-Math.exp(-0.12*txtCianuroVR));
     efectoDepresoresZinc = efectoZNS04Zinc + efectoCianuroZinc;
-    efectoCianuroFierro = (-7) * (1-Math.exp(-0.2*txtCianuroVR));
+    efectoCianuroFierro = (-7) * (1-Math.exp(-0.3*txtCianuroVR));
     efectoDepresoresFierro = efectoZNS04Fierro + efectoCianuroFierro;
     
     
